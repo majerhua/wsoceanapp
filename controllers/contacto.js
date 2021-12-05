b3c755f09b9ca2b17253bd5d8a3d5ee66a61b06a
majerhua <majerhua123@gmail.com>
2021-12-05 11:12:31 -0500

    heroku

---
+++ controllers/contacto.js
@@ -0,0 +1,39 @@
+const con = require('../conexion/conexion')
+
+deleteContact = (req, res) => {
+  let { id } = req.query
+
+  con.query(
+    `DELETE FROM contacto WHERE id = '${id}';`,
+    function (err, result, field) {
+        if (err) return res.status(500).send({ message: err.message, code: 0 })
+        return res.status(200).json({message:'El contacto se elimino correctamente',code: 1})
+    }
+  )
+}
+
+registerContact = (req,res)=>{
+    let {idUsuario,nombre,celular} = req.query || req.body;
+
+    con.query(
+      `SELECT *FROM contacto WHERE id_usuario = ${idUsuario}`,
+      function (err, result, field) {
+          if (err) return res.status(500).send({ message: err.message, code: 0 })
+          if(result && result.length <= 3){
+            con.query(
+              `INSERT INTO contacto(id_usuario,nombre,celular) VALUES('${idUsuario}','${nombre}','${celular}')`,
+              function (err, result, field) {
+                if (err) return res.status(500).send({ message: err.message, code: 0 })
+                res.status(200).json({message:'Se registro contacto correctamente',code: 1})
+              }
+            )
+          }
+          return res.status(200).json({message:'No se pudo registrar el usuario',code: 0})
+      }
+    )
+}
+
+module.exports = {
+  deleteContact,
+  registerContact
+}
--- controllers/user.js
+++ controllers/user.js
@@ -16,7 +16,7 @@ validateUser = (req, res) => {
 registerUser = (req,res)=>{
     let {nombres,apellidos,dni,celular,correo,direccion,fechaNacimiento,password} = req.query || req.body;
     con.query(
-        `INSERT INTO usuario VALUES('${nombres}','${apellidos}','${dni}','${celular}','${correo}','${direccion}','${fechaNacimiento}','${password}')`,
+        `INSERT INTO usuario(nombres,apellidos,dni,celular,correo,direccion,fecha_nacimiento,password) VALUES('${nombres}','${apellidos}','${dni}','${celular}','${correo}','${direccion}','${fechaNacimiento}','${password}')`,
         function (err, result, field) {
           if (err) return res.status(500).send({ message: err.message, code: 0 })
           res.status(200).json({message:'Se registro correctamente',code: 1})
