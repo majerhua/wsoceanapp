f9ef7c6ccd94099e66220dab85539525fe0c46aa
majerhua <majerhua123@gmail.com>
2021-12-06 17:58:53 -0500

    heroku

--- controllers/disciplina.js
+++ controllers/disciplina.js
@@ -28,7 +28,7 @@ listarDisciplina = (req, res) => {
   let {cliente} = req.query;
 
   con.query(
-    `SELECT *FROM appgimnasio_disciplina WHERE Estado = 1 AND Cliente = ${cliente}`,
+    `SELECT *FROM appgimnasio_disciplina WHERE Estado = 1 AND Cliente = '${cliente}'`,
     function (err, result, field) {
         if (err) return res.status(500).send({ message: err.message, code: 0 })
         return res.status(200).json({message:`lista de disciplinas`,code: 1,disciplinas: result})
