6239e80ce28d3d23941d6d16499521ec3c607078
majerhua <majerhua123@gmail.com>
2022-08-22 20:27:35 -0500

    add login

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-  host: 'r98du2bxwqkq3shg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
+  host: 'r98du2bxwqkq3shg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user: 'ae7fordo9y1qf6vn',
   password: 'jf8lrk89s4tii81q',
   database: 'yto229sn9he1o0tl',
