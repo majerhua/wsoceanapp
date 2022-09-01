82789b0e75e8e4632a7b9682b6630ba6963775ac
majerhua <majerhua123@gmail.com>
2022-08-31 20:26:11 -0500

    lance

--- controllers/lance.js
+++ controllers/lance.js
@@ -8,11 +8,11 @@ const register = (req, res) => {
     latitud,
     longitud,
     rumbo,
-    embarcacion,
-    matricula } = req.query
+    zarpe_id
+   } = req.query
   
   con.query(
-    `INSERT INTO lance(numeroLance, fechaLance, horaLance, latitud, longitud, rumbo, embarcacion, matricula) VALUES('${numeroLance}', '${fechaLance}', '${horaLance}', '${latitud}', '${longitud}', '${rumbo}', '${embarcacion}', '${matricula}')`,
+    `INSERT INTO lance(numeroLance, fechaLance, horaLance, latitud, longitud, rumbo, zarpe_id) VALUES('${numeroLance}', '${fechaLance}', '${horaLance}', '${latitud}', '${longitud}', '${rumbo}', ${zarpe_id})`,
     function (err, result, field) {
         if (err) return res.status(500).send({ message: err.message, code: 0 })
         return res.status(200).json({message:'Se registro correctamente',code: 1})
