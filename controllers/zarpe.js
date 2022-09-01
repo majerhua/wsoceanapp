5949553618576b647a56648376e3cd6fdaacfad5
majerhua <majerhua123@gmail.com>
2022-08-31 20:05:11 -0500

    zarpe

--- controllers/zarpe.js
+++ controllers/zarpe.js
@@ -1,8 +1,28 @@
 const con = require('../conexion/conexion')
 
+const register = (req, res) => {
+  let { 
+    embarcacion_id,
+    puertoZarpe,
+    fechaZarpe,
+    horaZarpe,
+    puertoArribo,
+    objetivo,
+    comentario } = req.query
+  
+  con.query(
+    `INSERT INTO zarpe(embarcacion_id, puertoZarpe, fechaZarpe, horaZarpe, puertoArribo, objetivo, comentario) VALUES('${embarcacion_id}', '${puertoZarpe}', '${fechaZarpe}', '${horaZarpe}', '${puertoArribo}', '${objetivo}', '${comentario}')`,
+    function (err, result, field) {
+        if (err) return res.status(500).send({ message: err.message, code: 0 })
+        return res.status(200).json({message:'Se registro correctamente',code: 1})
+    }
+  )
+}
+
+
 const get = (req, res) => {
   con.query(
-    `SELECT z.id, CONCAT(e.nombre,'-',z.id) nombre  FROM zarpe z INNER JOIN embarcacion e ON e.id = z.embarcacion_id`,
+    `SELECT z.id, CONCAT(e.nombre,'-',z.id) nombre, e.matricula  FROM zarpe z INNER JOIN embarcacion e ON e.id = z.embarcacion_id`,
     function (err, result, field) {
       if (err) return res.status(500).send({ message: err.message, code: 0 })
       return res.status(200).json(result)
@@ -11,5 +31,6 @@ const get = (req, res) => {
 }
 
 module.exports = {
+  register,
   get
 }
