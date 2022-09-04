ac310928a2b3ed5dcff9de1bc0a3a8717fb39ab1
majerhua <majerhua123@gmail.com>
2022-09-03 22:07:12 -0500

    add cala

--- controllers/lance.js
+++ controllers/lance.js
@@ -1,4 +1,5 @@
-const con = require('../conexion/conexion')
+const con = require('../conexion/conexion');
+const uid =  require('uid');
 
 const register = (req, res) => {
   let { 
@@ -10,9 +11,8 @@ const register = (req, res) => {
     rumbo,
     zarpe_id
    } = req.query
-  
   con.query(
-    `INSERT INTO lance(numeroLance, fechaLance, horaLance, latitud, longitud, rumbo, zarpe_id) VALUES('${numeroLance}', '${fechaLance}', '${horaLance}', '${latitud}', '${longitud}', '${rumbo}', ${zarpe_id})`,
+    `INSERT INTO lance(numeroLance, fechaLance, horaLance, latitud, longitud, rumbo, zarpe_id, cala) VALUES('${numeroLance}', '${fechaLance}', '${horaLance}', '${latitud}', '${longitud}', '${rumbo}', ${zarpe_id}, '${uid.uid(8)}')`,
     function (err, result, field) {
         if (err) return res.status(500).send({ message: err.message, code: 0 })
         return res.status(200).json({message:'Se registro correctamente',code: 1})
