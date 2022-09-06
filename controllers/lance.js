e0d4c0ad5f51bcebc2ccb8a0a80d5b15f9bb2a6f
majerhua <majerhua123@gmail.com>
2022-09-05 22:23:00 -0500

    change name cala to cala_id

--- controllers/lance.js
+++ controllers/lance.js
@@ -12,7 +12,7 @@ const register = (req, res) => {
     zarpe_id
    } = req.query
   con.query(
-    `INSERT INTO lance(numeroLance, fechaLance, horaLance, latitud, longitud, rumbo, zarpe_id, cala) VALUES('${numeroLance}', '${fechaLance}', '${horaLance}', '${latitud}', '${longitud}', '${rumbo}', ${zarpe_id}, '${uid.uid(8)}')`,
+    `INSERT INTO lance(numeroLance, fechaLance, horaLance, latitud, longitud, rumbo, zarpe_id, cala_id) VALUES('${numeroLance}', '${fechaLance}', '${horaLance}', '${latitud}', '${longitud}', '${rumbo}', ${zarpe_id}, '${uid.uid(8)}')`,
     function (err, result, field) {
         if (err) return res.status(500).send({ message: err.message, code: 0 })
         return res.status(200).json({message:'Se registro correctamente',code: 1})
