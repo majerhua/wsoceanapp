3ebaa9e04c0913d2254f03f221f0017c82f17e58
majerhua <majerhua123@gmail.com>
2022-09-18 23:35:37 -0500

    get all galery by user id

--- controllers/load-image.js
+++ controllers/load-image.js
@@ -57,9 +57,9 @@ const galeriaFotos = (req,res)=> {
 
 
 const galeriaFotosAll = (req,res)=> {
-  
+  const {user_id} = req.query;
   con.query(
-    `SELECT * FROM lance_imagen ORDER BY id DESC`,
+    `SELECT * FROM lance_imagen WHERE user_id = ${user_id} ORDER BY id DESC`,
     function (err, result, field) {
       if (err) return res.status(500).send({ message: err.message, code: 0 })
       return res.status(200).json(result)
