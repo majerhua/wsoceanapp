fe7035861cc842d8304ca6d9dd6a0cd5ccfb9d93
majerhua <majerhua123@gmail.com>
2022-12-09 17:35:37 -0500

    change conexion

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-  host: 'qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
+  host: 'qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user: 'jkwi9b0o1fmrr7rx',
   password: 'oro3mw3w807zh2wi',
   database: 'y0yvug9qat08g8ax',
